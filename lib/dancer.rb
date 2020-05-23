3ccf62867306b80d046f06de90b1f501e8239451
DAZUN XING <dazun.xing@gmail.com>
2020-05-23 08:09:25 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+class Dancer
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+  extend MetaDancing
+end
+
+# class Dancer
+#   extend FancyDance::ClassMethods
+#   include FancyDance::InstanceMethods
+# end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,19 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+class Kid
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+  
+  extend MetaDancing
+end
+
+# class Kid
+#   extend FancyDance::ClassMethods
+#   include FancyDance::InstanceMethods
+# end
